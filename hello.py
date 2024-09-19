d75b7d7ed5a0174956fe569c16f9e49abf48751c
ehz0ah <haozhe4547@gmail.com>
2024-09-19 19:21:00 +0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3 -> this tells the compiler that python3 is used to run this file
+print("Hello, World!")
+
+# To make file executable, use chmod +x hello.py
---
+++ squarearea.py
@@ -0,0 +1,6 @@
+
+#!usr/bin/env python3
+# To import this module, simply import {filename} e.g import squarearea
+# If you want a directory to be recognise as a module, you need to create a __init__.py file
+def square_area(side):
+    return side * side
