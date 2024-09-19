f6933a367817f5ba82e17ebe02c1d9ae99353c29
ehz0ah <haozhe4547@gmail.com>
2024-09-19 19:31:27 +0800

    Rearrange folder structure

--- scripts/squarearea.py
+++ scripts/squarearea.py
@@ -1,4 +1,3 @@
-
 #!usr/bin/env python3
 # To import this module, simply import {filename} e.g import squarearea
 # If you want a directory to be recognise as a module, you need to create a __init__.py file
