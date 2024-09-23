b2da85e20824f9c5be46e34463039ed58fe7c3d0
ehz0ah <haozhe4547@gmail.com>
2024-09-23 22:24:21 +0800

    Combine RegEx patterns

--- scripts/regex/escape.py
+++ scripts/regex/escape.py
@@ -10,3 +10,15 @@ print(re.search(r"\w*", "And_this_is_another"))  # \w matches any alphanumeric c
 # There's also \d for matching digits
 # \s for matching whitespace characters like space, tab or new line
 # \b for word boundaries
+
+import re
+def check_web_address(text):
+  pattern = r'^[\w\.\-\+]+\.[a-zA-Z]+$'
+  result = re.search(pattern, text)
+  return result != None
+
+print(check_web_address("gmail.com")) # True
+print(check_web_address("www@google")) # False
+print(check_web_address("www.Coursera.org")) # True
+print(check_web_address("web-address.com/homepage")) # False
+print(check_web_address("My_Favorite-Blog.US")) # True
